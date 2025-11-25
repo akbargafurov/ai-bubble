2dc12b1cd6aa85a9d018a6af3a7a6d0574f8478c
Akbar Gafurov <akbgafurov@gmail.com>
2025-11-25 17:12:53 +0100

    refactor: polish calculate_returns function

--- bubble/indicators.py
+++ bubble/indicators.py
@@ -2,27 +2,29 @@ import numpy as np
 import pandas as pd
 
 
-def calculate_returns(fin_data, log_returns=False):
+def calculate_returns(close, log_returns=False):
     """
-    Calculate daily returns from financial data on a percentage change basis.
+    Calculate daily returns from closing prices.
 
     Parameters:
-        fin_data (pd.DataFrame): pandas DataFrame of closing prices indexed by date.
-        log_returns (bool, optional): If True, calculates log returns instead of simple returns. Otherwise, defaults to False.
+        close (pd.DataFrame): pandas DataFrame of closing prices indexed by date.
+        log_returns (bool, optional): 
+            If True, calculates log returns instead of simple returns. 
+            Else, defaults to False.
 
     Returns:
         pd.DataFrame: pandas DataFrame of daily returns.
     """
 
     # handle cases where no data exists
-    if fin_data.empty:
+    if close.empty:
         raise ValueError(f"Input financial data is empty.")
 
-    # calculate daily percentage returns
+    # calculate daily returns (logarithmic or simple)
     if log_returns:
-        returns = fin_data.apply(lambda x: np.log(x) - np.log(x.shift(1))).dropna()
+        returns = np.log(close / close.shift(1)).dropna()
     else:
-        returns = fin_data.pct_change().dropna()
+        returns = close.pct_change().dropna()
 
     return returns
 
