6987168c3c438fc3c4c8c4dae41ac6f436ece3f2
Akbar Gafurov <akbgafurov@gmail.com>
2025-11-28 10:51:07 +0100

    feat: adjust the layout for plot_correlation_matrix function

--- bubble/visualizations.py
+++ bubble/visualizations.py
@@ -10,7 +10,8 @@ def plot_correlation_matrix(returns):
         returns (pd.DataFrame): pandas DataFrame of daily returns indexed by date.
     
     Returns:
-        None"""
+        None
+    """
 
     # plot correlation matrix of returns
     corr_matrix = returns.corr()
@@ -19,6 +20,7 @@ def plot_correlation_matrix(returns):
     plt.figure(figsize=(10, 8))
     sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', vmin=-1, vmax=1)
     plt.title('correlation matrix of AI stock returns')
+    plt.tight_layout()
     plt.show()
 
 
