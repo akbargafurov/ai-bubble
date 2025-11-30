112fdfa7ebb35684dd3c6cbd47b72a8562ee5af0
Akbar Gafurov <akbgafurov@gmail.com>
2025-11-30 17:20:32 +0100

    style: structure __init__.py alphabetically

--- bubble/__init__.py
+++ bubble/__init__.py
@@ -1,24 +1,25 @@
 from .loader import load_stock_data
 
 from .indicators import (
-    calculate_returns,
-    calculate_rolling_volatility,
-    calculate_rolling_correlation,
     build_equal_weight_index,
-    calculate_index_returns,
-    calculate_rolling_sharpe,
     calculate_drawdown,
-    calculate_max_drawdown
+    calculate_index_returns,
+    calculate_max_drawdown,
+    calculate_returns,
+    calculate_rolling_correlation,
+    calculate_rolling_sharpe,
+    calculate_rolling_volatility,
 )
 
 from .visualizations import (
     plot_correlation_matrix,
-    plot_rolling_correlation,
-    plot_normalized_prices,
-    plot_index_vs_benchmark,
-    plot_rolling_sharpe,
     plot_drawdown,
-    plot_return_distribution
+    plot_index_vs_benchmark,
+    plot_normalized_prices,
+    plot_return_distribution,
+    plot_rolling_correlation,
+    plot_rolling_sharpe,
+    plot_rolling_volatility,
 )
 
 __version__ = "0.1.0"
